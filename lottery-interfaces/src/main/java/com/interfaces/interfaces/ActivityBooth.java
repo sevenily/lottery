bb8b937d13fcc538b6ac769b383fb2ce8b25057c
闫红丹 <yanhongdan@hollysys.net>
2023-03-29 14:13:32 +0800

    新增lottery-interfaces 接口测试模块

---
+++ lottery-interfaces/src/main/java/com/interfaces/interfaces/ActivityBooth.java
@@ -0,0 +1,15 @@
+package com.interfaces.interfaces;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @ClassName: ActivityBooth
+ * @Description: 活动服务
+ * @Author: seven
+ * @CreateTime: 2023-03-29 13:33
+ * @Version: 1.0
+ **/
+@Service
+public class ActivityBooth {
+
+}
