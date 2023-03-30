0d3ffda33ebf466fc0d739c62eb5601aa35b9495
闫红丹 <yanhongdan@hollysys.net>
2023-03-30 09:29:53 +0800

    新增lottery-rpc 远程调用模块

---
+++ lottery-rpc/src/main/java/com/rpc/IActivityBooth.java
@@ -0,0 +1,20 @@
+package com.rpc;
+
+/**
+ * @ClassName: IActivityBooth
+ * @Description: 活动展台
+ * 1. 创建活动
+ * 2. 更新活动
+ * 3. 查询活动
+ * @Author: seven
+ * @CreateTime: 2023-03-29 14:16
+ * @Version: 1.0
+ **/
+
+
+public interface IActivityBooth {
+
+
+
+
+}
